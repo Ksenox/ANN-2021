908ab7506b438fb0936c9f43339634e5fdab64a7
Dmitriy129 <dmitriy77677@gmail.com>
2021-04-08 16:48:02 +0300

    fix(pr5)

--- 8383/Pereverzev/pr/5/gen.py
+++ 8383/Pereverzev/pr/5/gen.py
@@ -1,23 +1,45 @@
 import numpy as np
-from math import sin
-
-sizeStud = 700
-sizeValid = 700
-x = [-5, 10]
-e = [0, 0.03]
-dx = x[1]-x[0]
+import pandas as pd
 
 
-def v2_3(X):
-    return[(3*x[0]) + np.random.rand(1) * e[1] for x in X]
+def genData(size_of_dataset, inp_size):
+    dataset = np.zeros((size_of_dataset, inp_size))
+    dataset_y = np.zeros(size_of_dataset)
+    for i in range(size_of_dataset):
+        X = np.random.normal(-5, 10)
+        e = np.random.normal(0, 0.3)
+        dataset[i, :] = (
+            np.round(e - X**3),
+            np.round(e + np.log(np.abs(X))),
+            np.round(e + np.exp(X)),
+            np.round(e + 4 + X),
+            np.round(e + np.sqrt(np.abs(X)) - X),
+            np.round(e + X),
+        )
+        dataset_y[i] = np.round(e + (3*X))
+    return np.round(np.array(dataset), decimals=3), np.array(dataset_y)
 
 
-def gen(size):
-    localX = [(np.random.rand(1) * (x[1]-x[0]))+x[0]
-              for idx in range(size)]
-    localVal = v2_3(localX)
-    return np.hstack((np.asarray(localX), np.asarray(localVal)))
+def genFullData(inp_size):
+    x_train, y_train = genData(600, inp_size)
+    x_test, y_test = genData(200, inp_size)
+    mean = x_train.mean(axis=0)
+    std = x_train.std(axis=0)
+    x_train -= mean
+    x_train /= std
+    x_test -= mean
+    x_test /= std
 
+    y_mean = y_train.mean(axis=0)
+    y_std = y_train.std(axis=0)
+    y_train -= y_mean
+    y_train /= y_std
+    y_test -= y_mean
+    y_test /= y_std
 
-np.savetxt("stud.csv", gen(sizeStud), delimiter=",")
-np.savetxt("valid.csv", gen(sizeValid), delimiter=",")
+    pd.DataFrame(np.round(x_test, 3)).to_csv("./dataset/x_test.csv")
+    pd.DataFrame(np.round(y_test, 3)).to_csv("./dataset/y_test.csv")
+    pd.DataFrame(np.round(x_train, 3)).to_csv("./dataset/x_train.csv")
+    pd.DataFrame(np.round(y_train, 3)).to_csv("./dataset/y_train.csv")
+
+    return x_train, y_train, x_test, y_test
--- 8383/Pereverzev/pr/5/main.py
+++ 8383/Pereverzev/pr/5/main.py
@@ -1,56 +1,75 @@
-from keras.models import Model
-from keras.layers import Input, Dense
+from gen import genFullData
 import numpy as np
-from functions import *
+import pandas as pd
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense
 
-# input data
-studData = np.genfromtxt('stud.csv', delimiter=',')
-validData = np.genfromtxt('valid.csv', delimiter=',')
-
-# parse
-studX = studData[:, 1:]
-studVal = studData[:, :1]
-validX = validData[:, 1:]
-validVal = validData[:, :1]
-
-# encode
-encodedStudX = encode(studX)
-encodedValidX = encode(validX)
-
-# Layers
-# input
-firstInput = Input(shape=(1,), name='firstInput')
-# encoding
-encodingOutput = Dense(1, name="eo")(firstInput)
-# decoding
-decodingOutput = Dense(1, name="do")(encodingOutput)
-# regression
-regressionLayer = Dense(32, activation='relu')(encodingOutput)
-regressionOutput = Dense(1, name='regressionOutput')(regressionLayer)
+INP_SIZE = 6
 
 
-# main model
-model = Model(inputs=[firstInput], outputs=[
-              regressionOutput, encodingOutput, decodingOutput])
-model.compile(optimizer='RMSprop', loss='MeanSquaredError', metrics='MeanAbsoluteError')
-model.fit([studX], [studVal, encodedStudX, studX],
-          epochs=150, batch_size=10, validation_split=0, verbose=2)
-# Models
-# encoding
-encodingModel, encodingPrediction = modelCreate(
-    firstInput, encodingOutput, validX)
-encodingModel.save('models/encoding.h5')
-np.savetxt('results/encoding.csv', np.hstack(
-    (encodedValidX, encodingPrediction)), delimiter=',')
-# decoding
-decodingModel, decodingPrediction = modelCreate(
-    firstInput, decodingOutput, validX)
-decodingModel.save('models/decoding.h5')
-np.savetxt('results/decoding.csv', np.hstack(
-    (validX, decodingPrediction)), delimiter=',')
-# regression
-regressionModel, regressionPrediction = modelCreate(
-    firstInput, regressionOutput, validX)
-regressionModel.save('models/regression.h5')
-np.savetxt('results/regression.csv', np.hstack(
-    (validVal, regressionPrediction)), delimiter=',')
+def encoding():
+    main_input = Input(shape=(INP_SIZE,), name='main_input')
+    encoded = Dense(64, activation='relu')(main_input)
+    encoded = Dense(32, activation='relu')(encoded)
+    encoded = Dense(INP_SIZE, activation='linear')(encoded)
+    return main_input, encoded
+
+
+def decoding():
+    input_encoded = Input(shape=(INP_SIZE,), name='input_encoded')
+    decoded = Dense(32, activation='relu',
+                    kernel_initializer='normal')(input_encoded)
+    decoded = Dense(64, activation='relu')(decoded)
+    decoded = Dense(INP_SIZE, name="out_dec")(decoded)
+    return input_encoded, decoded
+
+
+def regression(inp):
+    predicted = Dense(64, activation='relu',
+                      kernel_initializer='normal')(inp)
+    predicted = Dense(32, activation='relu')(predicted)
+    predicted = Dense(16, activation='relu')(predicted)
+    predicted = Dense(1, name="out_main")(predicted)
+    return predicted
+
+
+def createModels():
+
+    main_input, encoded = encoding()
+    input_encoded, decoded = decoding()
+    predicted = regression(encoded)
+
+    encoded = Model(main_input, encoded, name="encoder_layer")
+    decoded = Model(input_encoded, decoded, name="decoder_layer")
+    predicted = Model(main_input, predicted, name="regr_layer")
+
+    return encoded, decoded, predicted
+
+
+def runModels(encoded, decoded, main_model):
+
+    x_train, y_train, x_test, y_test = genFullData(INP_SIZE)
+
+    main_model.compile(optimizer="adam", loss="mse", metrics=['mae'])
+
+    history = main_model.fit(x_train, y_train, epochs=40,
+                             batch_size=5, verbose=1, validation_data=(x_test, y_test))
+
+    encoded_result = encoded.predict(x_test)
+    decoded_result = decoded.predict(encoded_result)
+    main_result = main_model.predict(x_test)
+
+    decoded.save('./model/decoder.h5')
+    encoded.save('./model/encoder.h5')
+    main_model.save('./model/main.h5')
+
+    return encoded_result, decoded_result, main_result, history
+
+
+encoded, decoded, main_model = createModels()
+encoded_result, decoded_result, main_result, history = runModels(
+    encoded, decoded, main_model)
+
+pd.DataFrame(np.round(main_result, 3)).to_csv("./result/main.csv")
+pd.DataFrame(np.round(encoded_result, 3)).to_csv("./result/encoded.csv")
+pd.DataFrame(np.round(decoded_result, 3)).to_csv("./result/decoded.csv")
