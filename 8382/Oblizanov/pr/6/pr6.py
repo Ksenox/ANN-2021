d33fb0888515925a2a3f48d29ce7e00f3d7eefb9
the21composer <kirvant31580@gmail.com>
2021-04-20 17:51:02 +0300

    8382_Oblizanov_Alexandr_pr6

---
+++ 8382/Oblizanov/pr/6/pr6.py
@@ -0,0 +1,62 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import matplotlib.pyplot as plt
+from var1 import gen_data
+
+data, labels = gen_data()
+size = len(labels)
+test_split = size // 5
+
+encoder = LabelEncoder()
+encoder.fit(labels)
+labels = encoder.transform(labels)
+
+temp = list(zip(data, labels))
+np.random.shuffle(temp)
+data, labels = zip(*temp)
+data = np.asarray(data).reshape(size, 50, 50, 1)
+labels = np.asarray(labels).flatten()
+
+test_data = data[:test_split]
+test_labels = labels[:test_split]
+train_data = data[test_split:size]
+train_labels = labels[test_split:size]
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(50, 50, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, kernel_size=(2, 2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+H = model.fit(train_data, train_labels, epochs=12, batch_size=16,
+              validation_data=(test_data, test_labels))
+model.evaluate(test_data, test_labels)
+
+loss = H.history['loss']
+val_loss = H.history['val_loss']
+acc = H.history['accuracy']
+val_acc = H.history['val_accuracy']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+plt.clf()
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
