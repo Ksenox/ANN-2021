36ab3b9e176fb4ea6f3eeec4ef8d27588cf4c85c
the21composer <kirvant31580@gmail.com>
2021-05-14 23:08:55 +0300

    8382_Oblizanov_Alexandr_pr8_3

---
+++ 8382/Oblizanov/pr/8/pr8.py
@@ -0,0 +1,94 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import matplotlib.pyplot as plt
+from var1 import gen_data
+
+epochs = input("Enter epochs: ").split(' ')
+epochs = list(map(int, epochs))
+
+class CustomCallback(keras.callbacks.Callback):
+  def __init__(self, train_data, train_labels):
+    self.train_labels = train_labels
+    self.train_data = train_data
+  def on_epoch_end(self, epoch, logs=None):
+    if epoch in epochs:
+      y_train = self.train_labels
+      x_train = self.train_data
+      prediction = self.model.predict(x_train)
+      prediction = [1 if x >= 0.5 else 0 for x in prediction]
+      data = [0] * len(set(y_train))
+      length = [0] * len(set(y_train))
+      for i in range(len(prediction)):
+        data[y_train[i]] += float(prediction[i] == y_train[i])
+        length[y_train[i]] += 1.0
+      for i in range(len(data)):
+        data[i] /= length[i]
+      x = []
+      for i in range(len(data)):
+        x.append("Class " + str(i))
+      plt.bar(x, data, 0.3)
+      plt.xlabel('Classes')
+      plt.ylabel('Accuracy')
+      plt.title('Epochs #' + str(epoch))
+      plt.ylim((0, 1))
+      plt.savefig("epoch" + str(epoch))
+      plt.clf()
+
+data, labels = gen_data(800)
+size = len(labels)
+test_split = size // 5
+
+encoder = LabelEncoder()
+encoder.fit(labels)
+labels = encoder.transform(labels)
+
+temp = list(zip(data, labels))
+np.random.shuffle(temp)
+data, labels = zip(*temp)
+data = np.asarray(data).reshape(size, 50, 50, 1)
+labels = np.asarray(labels).flatten()
+
+test_data = data[:test_split]
+test_labels = labels[:test_split]
+train_data = data[test_split:size]
+train_labels = labels[test_split:size]
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(50, 50, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, kernel_size=(2, 2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+H = model.fit(train_data, train_labels, epochs=12, batch_size=16,
+              validation_split = 0.2, callbacks=[CustomCallback(train_data, train_labels)])
+model.evaluate(test_data, test_labels)
+
+loss = H.history['loss']
+val_loss = H.history['val_loss']
+acc = H.history['accuracy']
+val_acc = H.history['val_accuracy']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+plt.clf()
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
