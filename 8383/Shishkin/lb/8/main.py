1e4f52e705206d55796818f41df1ebb20b900a41
IvanShish <rayman.4@list.ru>
2021-05-27 09:39:43 +0300

    lb8

---
+++ 8383/Shishkin/lb/8/main.py
@@ -0,0 +1,95 @@
+# import os
+# os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+
+import numpy
+import sys
+# Load LSTM network and generate text
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.layers import Dense, Dropout, LSTM
+from keras.models import Sequential
+from keras.utils import np_utils
+from tensorflow import keras
+
+# load ascii text and covert to lowercase
+filename = "wonderland.txt"
+raw_text = open(filename).read()
+raw_text = raw_text.lower()
+
+# create mapping of unique chars to integers, and a reverse mapping
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+int_to_char = dict((i, c) for i, c in enumerate(chars))
+
+# summarize the loaded data
+n_chars = len(raw_text)
+n_vocab = len(chars)
+print("Total Characters: ", n_chars)
+print("Total Vocab: ", n_vocab)
+
+# prepare the dataset of input to output pairs encoded as integers
+seq_length = 100
+dataX = []
+dataY = []
+
+for i in range(0, n_chars - seq_length, 1):
+    seq_in = raw_text[i:i + seq_length]
+    seq_out = raw_text[i + seq_length]
+    dataX.append([char_to_int[char] for char in seq_in])
+    dataY.append(char_to_int[seq_out])
+
+n_patterns = len(dataX)
+print("Total Patterns: ", n_patterns)
+
+# reshape X to be [samples, time steps, features]
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+
+# normalize
+X = X / float(n_vocab)
+
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
+
+# define the LSTM model
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# save the network weights
+filepath = "weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+num_epochs = 20
+interval = 5
+
+
+class MyCallback(keras.callbacks.Callback):
+    def __init__(self):
+        super(MyCallback, self).__init__()
+
+    def on_epoch_end(self, epoch, logs=None):
+        if epoch == 0 or epoch == num_epochs - 1 or epoch % interval == 0:
+            # pick a random seed
+            start = numpy.random.randint(0, len(dataX) - 1)
+            pattern = dataX[start]
+            print("Seed:")
+            print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+
+            # generate characters
+            for i in range(50):
+                x = numpy.reshape(pattern, (1, len(pattern), 1))
+                x = x / float(n_vocab)
+                prediction = model.predict(x, verbose=0)
+                index = numpy.argmax(prediction)
+                result = int_to_char[index]
+                sys.stdout.write(result)
+                pattern.append(index)
+                pattern = pattern[1:len(pattern)]
+
+            print("\nDone.")
+
+
+tb_callback = TensorBoard(log_dir="./logs", histogram_freq=2, write_graph=True, embeddings_freq=1)
+callbacks_list = [checkpoint, MyCallback(), tb_callback]
+model.fit(X, y, epochs=num_epochs, batch_size=128, callbacks=callbacks_list)
